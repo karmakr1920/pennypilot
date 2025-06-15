1d5fb5ccb6e1d28df0108aacaff3fbb23dfd6cc3
Navin Kumar <naveen569652@gmail.com>
2025-06-15 18:13:07 +0530

    Fixed domain issue trailing slash

--- pennypilot/settings.py
+++ pennypilot/settings.py
@@ -19,7 +19,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://pennypilot.up.railway.app/',
+    'https://pennypilot.up.railway.app',
     
 ]
 
